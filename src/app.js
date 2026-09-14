eb5c01167dbdf2ce8ef0b4beecce82252d7bb5a5
Ranjeet Kumar <rkr.ranjeetkr@gmail.com>
2026-09-14 23:47:03 +0530

    feat: job creation api endpoint

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const candidateRoutes = require("./routes/candidate.routes");
+const jobRoutes = require("./routes/job.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/candidates", candidateRoutes);
+app.use("/jobs", jobRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
---
+++ src/routes/job.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+  createJob
+} = require("../controllers/job.controller");
+
+const router = express.Router();
+
+router.post("/", createJob);
+
+module.exports = router;
